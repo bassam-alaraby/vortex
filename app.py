aa6b3c0c8722cd69f56a88c9faddec27ad73f3ab
bassam-alaraby <elrabyb@gmail.com>
2026-04-04 06:19:38 +0200

    add Vortex branding

--- app.py
+++ app.py
@@ -20,32 +20,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/products")
-def products():
-    page = request.args.get("page")
-    try:
-        page = int(page)
-    except:
-        page = 1
-
-    if page < 1:
-        page = 1
-
-    per_page = 8
-    offset = (page - 1) * per_page
-
-    products = db.execute(
-        "SELECT * FROM products LIMIT ? OFFSET ?",
-        per_page, offset
-    )
-
-    row = db.execute("SELECT COUNT(*) AS count FROM products")
-    total_products = row[0]["count"]
-    total_pages = ceil(total_products / per_page)
-
-    return render_template(
-    "products.html",
-    products=products,
-    page=page,
-    total_pages=total_pages
-    )
+@app.route("/collection")
+def collection():
+    return render_template("collection.html")
