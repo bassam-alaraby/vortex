60b151b178efc980e0549a55926ed4d630e0538d
Bassam Tarek Al Araby <elrabyb@gmail.com>
2026-05-05 00:32:49 +0000

    test

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
 
-from database.init_db import BASE_DIR
-
-
 def load_env_file(file_path=".env"):
     if not os.path.exists(file_path):
         return
@@ -38,7 +35,7 @@ class Config:
     SECRET_KEY = _secret_key
     
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    DATABASE_PATH = os.environ.get('DATABASE_PATH', os.path.join(BASE_DIR, 'data', 'app.db'))
+    DATABASE_PATH = os.environ.get('DATABASE_PATH', 'sqlite:////app/data/app.db')
     
     TEMPLATES_AUTO_RELOAD = True
     
